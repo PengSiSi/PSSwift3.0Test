c87d617baf867ac3f5184b8852a1d0a0e88d167f
PengSiSi <15300086553@163.com>
2017-04-10 15:22:18 +0800

    自动引用计数

--- PSSwift3.0Test/68.自动引用计数/AutomCountViewController.swift
+++ PSSwift3.0Test/68.自动引用计数/AutomCountViewController.swift
@@ -39,6 +39,23 @@ class AutomCountViewController: BaseViewController {
         // 解决实例之间的循环强引用 
         /*Swift 提供了两种办法用来解决你在使用类的属性时所遇到的循环强引用问题：弱引用（weak reference）和无主引用（unowned reference）。
           对于生命周期中会变为nil的实例使用弱引用。相反地，对于初始化赋值后再也不会被赋值为nil的实例，使用无主引用。*/
+        // 总结: 
+        // 1.两个属性的值都允许为nil,弱引用来解决;
+        // 2.一个属性的值允许为nil，而另一个属性的值不允许为nil，这也可能会产生循环强引用。这种场景最适合通过无主引用来解决.
+        // 3.两个属性都必须有值，并且初始化完成后永远不会为nil。在这种场景中，需要一个类使用无主属性，而另外一个类使用隐式解析可选属性。
+        
+        let heading = HTMLElement(name: "h1")
+        let defaultText = "some default text"
+        heading.asHTML = {
+            return "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+        }
+        print(heading.asHTML)
+        
+        // 总结:
+        // 1.在闭包和捕获的实例总是互相引用并且总是同时销毁时，将闭包内的捕获定义为无主引用。
+        // 2.在被捕获的引用可能会变为nil时，将闭包内的捕获定义为弱引用
+        // 3.如果被捕获的引用绝对不会变为nil，应该用无主引用，而不是弱引用
+        
     }
 }
 
@@ -61,8 +78,33 @@ class Apartment {
     init(unit: String) {
         self.unit = unit
     }
-    var tenant: Personer?
+    weak var tenant: Personer?  // 弱引用
     deinit {
         print("Apartment \(unit) is being deinitialized")
     }
+}
+
+// 闭包引起的循环强引用
+class HTMLElement {
+    
+    let name: String
+    let text: String?
+    
+    lazy var asHTML: (Void) -> String = { [unowned self] in
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+    
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+    
 }
